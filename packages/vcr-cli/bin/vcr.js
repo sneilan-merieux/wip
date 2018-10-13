d4ed5e0830b164c52691246ccf35643e94a6aa5d
Wei Zhu <yesmeck@gmail.com>
2018-10-13 18:30:47 +0800

    change config name

--- packages/vcr-cli/bin/vcr.js
+++ packages/vcr-cli/bin/vcr.js
@@ -2,6 +2,8 @@
 
 const config = require('../lib/config').default;
 
+process.env.JEST_PUPPETEER_CONFIG = 'vcr.config.js';
+
 require('jest-cli/build/cli').run([
   '--config',
   JSON.stringify(config)
