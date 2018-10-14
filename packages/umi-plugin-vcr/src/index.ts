8edba9c4d24b00fdf911e5ffcb388de15e4d8fd3
Wei Zhu <yesmeck@gmail.com>
2018-10-14 17:30:02 +0800

    Fix umi plugin

--- packages/umi-plugin-vcr/src/index.ts
+++ packages/umi-plugin-vcr/src/index.ts
@@ -1,12 +1,7 @@
-import VcrWebpackPlugin from 'vcr-webpack-plugin';
 import vcrMiddleware from 'vcr-webpack-plugin/lib/middleware';
 
 export default (api) => {
-  api.chainWebpackConfig((config) => {
-    config.plugin('vcr').use(VcrWebpackPlugin);
-  });
-
-  api.addMiddlewareAhead(() => {
-    return vcrMiddleware;
+  api._beforeServerWithApp(function ({ app }) {
+    vcrMiddleware(app);
   });
 }
