bfe4bcbb47ccf6566f60832b044eab8db456dfaa
Wei Zhu <yesmeck@gmail.com>
2018-10-14 17:53:44 +0800

    clean before build

--- packages/vcr-webpack-plugin/src/middleware.ts
+++ packages/vcr-webpack-plugin/src/middleware.ts
@@ -9,12 +9,12 @@ export default app => {
   app.get('/__vcr__/client.bundle.js', (_req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
 
-    fs.createReadStream(require.resolve('vcr-client/dist/index.bundle.js')).pipe(res);
+    fs.createReadStream(require.resolve('vcr-client/dist/client.bundle.js')).pipe(res);
   });
 
   app.get('/__vcr__/sw.bundle.js', (_req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
 
-    fs.createReadStream(require.resolve('vcr-client/dist/index.bundle.js')).pipe(res);
+    fs.createReadStream(require.resolve('vcr-client/dist/sw.bundle.js')).pipe(res);
   });
 };
